bcae3af8d62a30a2a109b0281da8d5fc7a18c5e6
AngelNovelli <angeljesusnovelli@gmail.com>
2024-07-28 22:56:38 -0300

    tercer commit, va tomando forma

--- contacto.js
+++ contacto.js
@@ -2,6 +2,7 @@ let nombre = document.getElementById("nombre");
 let apellido = document.getElementById("apellido");
 let email = document.getElementById("email");
 let tel = document.getElementById("tel");
+let mensaje = document.getElementById("mensaje");
 let btnEnviar = document.getElementById("enviar");
 let informacion = [];
 
@@ -11,9 +12,10 @@ btnEnviar.addEventListener("click", (e) => {
     informacion[1] = apellido.value;
     informacion[2] = email.value;
     informacion[3] = tel.value;
+    informacion[4] = mensaje.value;
     
 
-    console.log(`Su nombre es ${informacion[0]} y su apellido es ${informacion[1]}, ${informacion[2]}, ${informacion[4]}`);
+    console.log(`Su nombre es ${informacion[0]} y su apellido es ${informacion[1]}, ${informacion[2]},${informacion[3]}, ${informacion[4]}`);
 
     let blob = new Blob([informacion], {type: "text/plain;charset=utf-8"}); // navegador
 
